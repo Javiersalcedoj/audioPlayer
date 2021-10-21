ed04cb5a3623d89921d6a4c89e06d4f97e31464c
Javier Salcedo <javierss.2603@gmail.com>
2021-10-21 13:34:55 -0500

    add pluging timeline

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import { AudioPlayer } from "./AudioPlayer.js";
 
-let media;
-const music = new Audio ('./audio/ZITTI E BUONI.mp3')
 
+const music = new Audio ('./audio/ZITTI E BUONI.mp3')
+let media = new AudioPlayer (music);
 music.addEventListener('canplaythrough', function() {
-    media = new AudioPlayer (music);
     media.duration();
 })
 music.addEventListener('ended', ()=>{media.ended()})
 
 const timerDuration = document.querySelector('.timer--duration');
 const timerCurrent =  document.querySelector('.timer--current');
+music.addEventListener('timeupdate', refreshTimer) 
+
+function refreshTimer(){
+    media.current();
+    media.timeline();
+}
+function refreshTimerDrag () {
+    media.current();
+}
+
 
 const timeline = document.querySelector('.audioPlayer__timeline');
 timeline.addEventListener('click', ()=>{media.timelineClick()})
 const currentTimeline = document.querySelector('.timeline--highlight');
 
-music.addEventListener('timeupdate', ()=>{
-    media.current();
-    media.timeline();
-})
+const circleCureentTimeline = document.querySelector('.timeline--highlight--circle');
+circleCureentTimeline.addEventListener('dragstart', ()=>{
+    music.removeEventListener('timeupdate', refreshTimer);
+    music.addEventListener('timeupdate', refreshTimerDrag);
+});
+circleCureentTimeline.addEventListener('drag', () => {media.dragTimeline()});
+circleCureentTimeline.addEventListener('dragend', ()=>{
+    media.timelineClick()
+    music.removeEventListener('timeupdate', refreshTimerDrag);
+    music.addEventListener('timeupdate', refreshTimer);
+});
 
 const back = document.querySelector('.button--back');
 back.addEventListener('click', ()=>{media.forwardBackward(false)})
